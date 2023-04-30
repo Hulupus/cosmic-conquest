4e0241ab4d217c5b2732607608fab8c47e8c9bc0
Kamal <kamal.mari@iserv-schiller-schule.de>
2023-04-30 15:28:00 +0200

    Level

--- Level.java
+++ Level.java
@@ -10,4 +10,33 @@ public class Level
     //Weiteren Vorgehen:
     //Logik zum Spawning?
     //->Pixelgröße als Lösung?
+    
+    private int[][] CruiserPos;
+    private int[][] BomberPos;
+    
+    int LevelNummer;
+    
+    public void Level(int pLevelNummer, int CruiserAnzahl, int BomberAnzahl, int pCruiserPos[][], int pBomberPos[][]){
+        LevelNummer = pLevelNummer;
+        
+        CruiserPos = new int[CruiserAnzahl][2]; // Array mit der Länge der Anzahl Gegner des Types Cruiser
+        for (int i = 0; i < CruiserPos.length; i++){
+            CruiserPos[i][1] = pCruiserPos[i][1];
+            CruiserPos[i][2] = pCruiserPos[i][2];
+        }
+        BomberPos = new int[BomberAnzahl][2]; // Array mit der Länge der Anzahl Gegner des Types Bomber
+        for (int i = 0; i < BomberPos.length; i++){
+            BomberPos[i][1] = pBomberPos[i][1];
+            BomberPos[i][2] = pBomberPos[i][2];
+        }
+    }
+    
+    public void oeffneLevel(){
+        for (int i = 0; i < CruiserPos.length; i++){
+            Cruiser[i].moveTo(CruiserPos[i][1], CruiserPos[i][2]);
+        }
+        for (int i = 0; i < BomberPos.length; i++){
+            Bomber[i].moveTo(BomberPos[i][1], BomberPos[i][2]);
+        }
+    }
 }
