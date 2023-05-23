920056f098b81d792b60c46399c5eeb25e537446
Hulupus <michael.vanveen@gmx.de>
2023-05-23 13:27:56 +0200

    base package hinzugefügt

---
+++ base/Building.java
@@ -0,0 +1,14 @@
+package base;
+/**
+ * Beschreiben Sie hier die Klasse Bulding.
+ * 
+ * @author (Ihr Name) 
+ * @version (eine Versionsnummer oder ein Datum)
+ */
+public class Building{
+    public Building()
+    {
+        
+    }
+
+}
