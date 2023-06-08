82ca027947172b7b9e35e459aa7d243966a979e9
Kamal <kamal.mari@iserv-schiller-schule.de>
2023-06-08 17:02:35 +0200

    Base Update

--- base/Building.java
+++ base/Building.java
@@ -1,18 +1,17 @@
 package base;
 
 import sas.*;
-/**
- * Beschreiben Sie hier die Klasse Bulding.
- * 
- * @author (Ihr Name) 
- * @version (eine Versionsnummer oder ein Datum)
- */
 public class Building{
     private Picture building;
     private int building_degree;
+    private int[] requiredParts;
     
-    public Building(String pictureURL, int xPos, int yPos, int height, int width){
+    public Building(String pictureURL, int xPos, int yPos, int height, int width, int[] prequiredParts){
         building = new Picture(xPos, yPos, height, width, pictureURL);
+        requiredParts = new int[6];
+        for (int i = 0; i < requiredParts.length; i++){
+            prequiredParts[i] = requiredParts[i];
+        }
     }
 
     public Boolean clicked(){
--- base/Laboratory.java
+++ base/Laboratory.java
@@ -6,7 +6,7 @@ public class Laboratory extends Building{
 
     public Laboratory(int pbuilding_degree)
     {
-        super("laboratory.png(WIP)", 400, 800, 20, 20);
+        super("laboratory.png(WIP)", 400, 800, 20, 20,  new int[] {3,6,1,1,7,0});
         
     }
     
